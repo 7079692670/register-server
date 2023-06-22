ffe5bb90659b996a0d7883ad0a5d25abaa8ac69a
7079692670 <sujit@prpservices.in>
2023-06-22 12:57:12 +0530

    Testing Code Through pipeLine

--- src/main/java/com/common/ServiceRegistry/ServiceRegistry/ServiceRegistryApplication.java
+++ src/main/java/com/common/ServiceRegistry/ServiceRegistry/ServiceRegistryApplication.java
@@ -10,6 +10,9 @@ public class ServiceRegistryApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ServiceRegistryApplication.class, args);
+		int value=100/0;
+		System.out.println("After Dvided valu: "+value);
 	}
+
 
 }
